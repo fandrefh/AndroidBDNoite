4dade928d1205ef426a987811d76dc74bdfe9927
Francisco André <fandrefh@gmail.com>
2015-11-19 20:03:09 -0200

    Listagem SQLite e ListView

---
+++ app/src/main/java/br/senac/pi/carros/ListaCarrosActivity.java
@@ -0,0 +1,63 @@
+package br.senac.pi.carros;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+import br.senac.pi.carros.domain.CarrosDB;
+
+public class ListaCarrosActivity extends AppCompatActivity {
+
+    private CarrosDB carrosDB;
+    private SQLiteDatabase database;
+    private CursorAdapter dataSource;
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_carros);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        carrosDB = new CarrosDB(this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        listView = (ListView) findViewById(R.id.listView);
+        findViewById(R.id.listarCarros).setOnClickListener(listarCarros());
+    }
+
+    private View.OnClickListener listarCarros() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                database = carrosDB.getReadableDatabase();
+                String[] campos = {"_id", "nome", "marca"};
+                Cursor cursor = database.query("carro", campos, null, null, null, null, null);
+                if (cursor.getCount() > 0) {
+                    dataSource = new SimpleCursorAdapter(getApplicationContext(), R.layout.item_lista, cursor, campos, new int[] {R.id.txtNomeCarro, R.id.txtMarcaCarro}, 0);
+                    listView.setAdapter(dataSource);
+                } else {
+                    Toast.makeText(getApplicationContext(), getString(R.string.zero_registro), Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+    }
+
+}
--- app/src/main/java/br/senac/pi/carros/MainActivity.java
+++ app/src/main/java/br/senac/pi/carros/MainActivity.java
@@ -1,17 +1,21 @@
 package br.senac.pi.carros;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.IOException;
 
 import br.senac.pi.carros.domain.CarrosDB;
 
@@ -67,7 +71,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        database.close();
+        try {
+            database.close();
+        } catch (NullPointerException e) {
+            Log.i("curso", "Babou geral: " + e.getMessage());
+        }
     }
 
     @Override
@@ -86,6 +94,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, ListaCarrosActivity.class);
+            startActivity(intent);
             return true;
         }
 
