ed946f7aafe2ee7e76e7cbf0dcd99a5684ee1a1a
Francisco André <fandrefh@gmail.com>
2015-11-25 18:28:22 -0200

    Corrigindo contexto AlertDialog

--- app/src/main/java/br/senac/pi/carros/ListaCarrosActivity.java
+++ app/src/main/java/br/senac/pi/carros/ListaCarrosActivity.java
@@ -1,5 +1,8 @@
 package br.senac.pi.carros;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -8,6 +11,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -21,6 +25,7 @@ public class ListaCarrosActivity extends AppCompatActivity {
     private SQLiteDatabase database;
     private CursorAdapter dataSource;
     private ListView listView;
+    private String[] campos = {"nome", "marca", "_id"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,7 @@ public class ListaCarrosActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         carrosDB = new CarrosDB(this);
+        database = carrosDB.getReadableDatabase();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -42,11 +48,11 @@ public class ListaCarrosActivity extends AppCompatActivity {
         });
         listView = (ListView) findViewById(R.id.listView);
         listarCarros();
+        //Click item da lista
+        listView.setOnItemClickListener(deletarOuEditarItem());
     }
 
     private void listarCarros() {
-        database = carrosDB.getReadableDatabase();
-        String[] campos = {"nome", "marca", "_id"};
         Cursor cursor = database.query("carro", campos, null, null, null, null, null);
         if (cursor.getCount() > 0) {
             dataSource = new SimpleCursorAdapter(getApplicationContext(), R.layout.item_lista, cursor, campos, new int[] {R.id.txtNomeCarro, R.id.txtMarcaCarro}, 0);
@@ -56,4 +62,37 @@ public class ListaCarrosActivity extends AppCompatActivity {
         }
     }
 
+    private AdapterView.OnItemClickListener deletarOuEditarItem() {
+        return new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final long itemSelecionado = id;
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListaCarrosActivity.this);
+                builder.setTitle(getString(R.string.titulo_alert));
+                builder.setMessage(getString(R.string.menssagem_alert));
+                builder.setNegativeButton(getString(R.string.opcao_delete), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        database.delete("carro", "_id = " + itemSelecionado, null);
+                        finish();
+                        Intent intent = new Intent(getApplicationContext(), ListaCarrosActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                builder.setPositiveButton(getString(R.string.opcao_editar), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Clicou no Editar", Toast.LENGTH_LONG).show();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        };
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        database.close();
+    }
 }
